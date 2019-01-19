6ac2f9d6232e5e2dd18614e15f0eb52262b67e1b
Jeroen Claassens <jeroen.claassens@live.nl>
2019-01-19 16:52:21 +0100

    Resolve last couple of issues

--- test/getPrices.spec.ts
+++ test/getPrices.spec.ts
@@ -1,4 +1,5 @@
 import { getPrices } from '../lib';
+import { US_GAME_CHECK_CODE } from '../lib/constants';
 
 test('getPrices', async () => {
     const expectedReturn = {
@@ -6,7 +7,7 @@ test('getPrices', async () => {
         personalized: false,
         prices: [{regular_price: {amount: '$39.99', currency: 'USD', raw_value: '39.99'}, sales_status: 'onsale', title_id: 70010000000185}],
     };
-    const data = await getPrices('US', '70010000000025');
+    const data = await getPrices('US', US_GAME_CHECK_CODE);
     expect(data).toBeInstanceOf(Object);
     expect(data).toMatchObject(expectedReturn);
 });
