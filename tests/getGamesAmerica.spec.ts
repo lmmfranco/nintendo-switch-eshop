26a5a4b85bade13b1b07f637199b1ab3a4ad7163
Jeroen Claassens <jeroen.claassens@live.nl>
2019-02-26 12:17:36 +0100

    Write test that covers looping over getGamesAmerica

--- tests/getGamesAmerica.spec.ts
+++ tests/getGamesAmerica.spec.ts
@@ -1,7 +1,17 @@
 import { getGamesAmerica } from '../lib';
 
-test('getGamesAmerica', async () => {
-    const data = await getGamesAmerica({shop: 'ncom', limit: 1});
-    expect(data).toBeInstanceOf(Object);
-    expect(data).toHaveLength(1);
+describe('getGamesAmerica', () => {
+    it('should allow custom limit', async () => {
+        const data = await getGamesAmerica({shop: 'ncom', limit: 1});
+        expect(data).toBeInstanceOf(Object);
+        expect(data).toHaveLength(1);
+    });
+
+    it('should allow unfiltered shop', async () => {
+        jest.setTimeout(60000);
+        const data = await getGamesAmerica({shop: 'unfiltered'});
+        expect(data).toBeInstanceOf(Object);
+        expect(data.length).toBeGreaterThanOrEqual(1500);
+    });
 });
+
