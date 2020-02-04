96d9ace13ae2f54fc121e7335ca16e9564001e33
Jeroen Claassens <support@favware.tech>
2020-02-04 22:23:42 +0100

    test: remove shop property from JS based test

--- __tests__/commonJs.test.js
+++ __tests__/commonJs.test.js
@@ -3,14 +3,14 @@ const { getGamesAmerica } = require('../src');
 
 describe('getGamesAmerica - CommonJS', () => {
   test('should allow custom limit', async () => {
-    const data = await getGamesAmerica({shop: 'ncom', limit: 1});
+    const data = await getGamesAmerica({limit: 1});
     expect(data).toBeInstanceOf(Object);
     expect(data).toHaveLength(1);
   });
 
   test('should allow unfiltered shop', async () => {
     jest.setTimeout(60000);
-    const data = await getGamesAmerica({shop: 'unfiltered'});
+    const data = await getGamesAmerica();
     expect(data).toBeInstanceOf(Object);
     expect(data.length).toBeGreaterThanOrEqual(1500);
   });
