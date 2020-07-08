a444e5e5ff7e38481e8f8833e61b98bd19666131
Jeroen Claassens <support@favware.tech>
2020-07-08 15:40:47 +0200

    test: setup test retries and timeout

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
 	testEnvironment: 'node',
 	testRunner: 'jest-circus/runner',
 	testMatch: ['<rootDir>/__tests__/*.test.ts'],
+	setupFilesAfterEnv: ['<rootDir>/__tests__/jest.setup.ts'],
 	globals: {
 		'ts-jest': {
 			tsConfig: '<rootDir>/__tests__/tsconfig.json'
