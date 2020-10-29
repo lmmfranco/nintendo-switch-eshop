b8c59d9b90fc41862e2936d65b7ddf25a0a6fedc
Bryan Van Zile <bryanvanzile@gmail.com>
2020-10-28 20:39:45 -0400

    fix(api): updated getGamesAmerica algolia request to retrieve all games
    getGamesAmerica will now always get all data and it has lost some
    parameters in the process.
    
    BREAKING CHANGE: getGamesAmerica no longer takes any parameters and
    should always return all games.

--- __tests__/getGamesAmerica.test.ts
+++ __tests__/getGamesAmerica.test.ts
@@ -1,11 +1,12 @@
 import defaultGetGamesAmerica, { getGamesAmerica } from '../src';
 
 describe('getGamesAmerica', () => {
-  test('should allow custom limit', async () => {
-    const data = await getGamesAmerica({ limit: 1 });
-    expect(data).toBeInstanceOf(Object);
-    expect(data).toHaveLength(1);
-  });
+  // disabled test currently due to re-write of getGamesAmerica to return all games only, no options
+  // test('should allow custom limit', async () => {
+  //   const data = await getGamesAmerica({ limit: 1 });
+  //   expect(data).toBeInstanceOf(Object);
+  //   expect(data).toHaveLength(1);
+  // });
 
   test('should allow no options', async () => {
     jest.setTimeout(60000);
