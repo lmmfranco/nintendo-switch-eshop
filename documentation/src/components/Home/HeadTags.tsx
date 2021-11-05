ab08cc8f8bd832fde2b3043a90823e23540d00a2
Jeroen Claassens <support@favware.tech>
2021-11-05 22:25:43 +0100

    docs: fixed various meta tags

--- documentation/src/components/Home/HeadTags.tsx
+++ documentation/src/components/Home/HeadTags.tsx
@@ -1,7 +1,10 @@
 import Head from '@docusaurus/Head';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import React, { FC, memo } from 'react';
 
 const HeadTags: FC = () => {
+  const { siteConfig } = useDocusaurusContext();
+
   return (
     <Head>
       <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
@@ -14,14 +17,14 @@ const HeadTags: FC = () => {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;700&display=swap" rel="stylesheet" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/img/favicons/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="192x192" href="/img/favicons/android-chrome-192x192.png" />
-      <link rel="icon" type="image/png" sizes="194x194" href="/img/favicons/android-chrome-194x194.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/img/favicons/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/img/favicons/favicon-16x16.png" />
-      <link rel="manifest" href="/manifest.webmanifest" />
-      <link rel="mask-icon" href="/img/favicons/safari-pinned-tab.svg" color="#F89034" />
-      <link rel="shortcut icon" href="/img/favicons/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href={`${siteConfig.baseUrl}img/favicons/apple-touch-icon.png`} />
+      <link rel="icon" type="image/png" sizes="192x192" href={`${siteConfig.baseUrl}img/favicons/android-chrome-192x192.png`} />
+      <link rel="icon" type="image/png" sizes="194x194" href={`${siteConfig.baseUrl}img/favicons/favicon-194x194`} />
+      <link rel="icon" type="image/png" sizes="32x32" href={`${siteConfig.baseUrl}img/favicons/favicon-32x32.png`} />
+      <link rel="icon" type="image/png" sizes="16x16" href={`${siteConfig.baseUrl}img/favicons/favicon-16x16.png`} />
+      <link rel="manifest" href={`${siteConfig.baseUrl}/manifest.webmanifest`} />
+      <link rel="mask-icon" href={`${siteConfig.baseUrl}/img/favicons/safari-pinned-tab.svg`} color="#F89034" />
+      <link rel="shortcut icon" href={`${siteConfig.baseUrl}img/favicons/favicon.ico`} />
       <link rel="apple-touch-startup-image" href="/img/favicons/apple-startup.png" />
     </Head>
   );
